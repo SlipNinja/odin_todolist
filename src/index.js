3ecac2fe079ab3306b5547eae8fcde3c164d5e2a
Slipninja <26084911+SlipNinja@users.noreply.github.com>
2023-03-26 19:49:07 +0200

    Add project's basic structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//import buildPage from './pageBuilder';
+import './style.css';
+
+const test = document.createElement("div");
+test.innerHTML = "EVERYTHING IS FINE";
+
+document.body.appendChild(test);
